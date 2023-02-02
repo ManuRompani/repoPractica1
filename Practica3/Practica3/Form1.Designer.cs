38f65aa7dd76eddf306703d522e2ede3b410ea1a
ManuRompani <manuelrompani14@gmail.com>
2023-02-02 18:11:05 -0300

    primera subida...

---
+++ Practica3/Practica3/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Practica3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFont = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.grpFont = new System.Windows.Forms.GroupBox();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.grpFont.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblFont
+            // 
+            this.lblFont.AutoSize = true;
+            this.lblFont.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFont.Location = new System.Drawing.Point(95, 28);
+            this.lblFont.Name = "lblFont";
+            this.lblFont.Size = new System.Drawing.Size(124, 20);
+            this.lblFont.TabIndex = 0;
+            this.lblFont.Text = "Texto de prueba";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(75, 62);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(169, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Microsoft Sans Serif";
+            this.comboBox1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.comboBox1_DrawItem);
+            // 
+            // grpFont
+            // 
+            this.grpFont.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.grpFont.Controls.Add(this.btnChange);
+            this.grpFont.Controls.Add(this.comboBox1);
+            this.grpFont.Controls.Add(this.lblFont);
+            this.grpFont.Location = new System.Drawing.Point(219, 117);
+            this.grpFont.Name = "grpFont";
+            this.grpFont.Size = new System.Drawing.Size(332, 170);
+            this.grpFont.TabIndex = 2;
+            this.grpFont.TabStop = false;
+            this.grpFont.Text = "Text edit";
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(251, 62);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(57, 21);
+            this.btnChange.TabIndex = 2;
+            this.btnChange.Text = "Change";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 408);
+            this.Controls.Add(this.grpFont);
+            this.MaximumSize = new System.Drawing.Size(806, 447);
+            this.MinimumSize = new System.Drawing.Size(806, 447);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.grpFont.ResumeLayout(false);
+            this.grpFont.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFont;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox grpFont;
+        private System.Windows.Forms.Button btnChange;
+    }
+}
+
---
+++ Practica3/Practica3/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            foreach (FontFamily font in FontFamily.Families)
+            {
+                string nombre = font.Name;
+                comboBox1.Items.Add(nombre);
+            }
+        }
+
+        private void comboBox1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            string text = comboBox1.Items[e.Index].ToString();
+            Font font = new Font(text, e.Font.Size);
+
+            e.Graphics.DrawString(text, font, new SolidBrush(e.ForeColor), e.Bounds.Left + 2, e.Bounds.Top + 2);
+
+            e.DrawFocusRectangle();
+        }
+    }
+}
